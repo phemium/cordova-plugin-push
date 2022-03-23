dc2a9002bdc6003ed9d9237ab00dd6f2c053f739
Phemium <devel@phemium.com>
2022-03-23 09:42:37 +0100

    Update plugin packages

--- src/android/com/adobe/phonegap/push/CordovaCall.java
+++ src/android/com/adobe/phonegap/push/CordovaCall.java
@@ -1,4 +1,4 @@
-package com.dmarc.cordovacall;
+package com.adobe.phonegap.push;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
--- src/android/com/adobe/phonegap/push/MyConnectionService.java
+++ src/android/com/adobe/phonegap/push/MyConnectionService.java
@@ -1,4 +1,4 @@
-package com.dmarc.cordovacall;
+package com.adobe.phonegap.push;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
