afe0e25e12191cb9d87e8edcd3b3002e52294d4c
Phemium <devel@phemium.com>
2023-05-05 12:25:55 +0200

    Update beforeCompile.js

--- hooks/android/beforeCompile.js
+++ hooks/android/beforeCompile.js
@@ -1,9 +1,16 @@
 const { join } = require("path");
 const { existsSync, readFileSync, writeFileSync } = require("fs");
+const { promisify } = require("util");
 const { parseElementtreeSync } = require("cordova-common/src/util/xml-helpers");
 const platform = require("cordova-android");
+const _glob = require("glob");
+const glob = promisify(_glob);
 
-module.exports = function (context) {
+module.exports = async function (context) {
+  context = {};
+  context.opts = {};
+  context.opts.projectRoot = "/Users/alex/Documents/Projects/cigna/cigna-web-app/src";
+  await configurePEM(context);
   if (!isExecutable()) {
     console.log("[cordova-plugin-push::before-compile] skipping before_compile hookscript.");
     return;
@@ -55,4 +62,56 @@ function updateBuildGradle(context, buildGradleFilePath) {
   console.log(
     `[cordova-plugin-push::before-compile] updated "build.gradle" file with kotlin version set to: ${kotlinVersion}`
   );
+}
+
+async function configurePEM(context) {
+  console.log("[cordova-plugin-push::before-compile] Configuring PEM integration");
+  const pemDir = join(
+    context.opts.projectRoot,
+    "platforms/android/app/src/main/java/com/phemium/plugins/PhemiumEnduserPlugin.java"
+  );
+  const pemIntegration = existsSync(pemDir);
+  if (pemIntegration) {
+    console.log(
+      "[cordova-plugin-push::before-compile] PEM detected, enabling PEM feature in plugin"
+    );
+  } else {
+    console.log(
+      "[cordova-plugin-push::before-compile] PEM not detected, disabling PEM feature in plugin"
+    );
+  }
+  let files = await glob(
+    join(context.opts.projectRoot, "platforms/android/app/src/main/java/com/adobe/phonegap/push") +
+      "/**/*.kt"
+  );
+  for (const file of files) {
+    let content = readFileSync(file, { encoding: "utf-8" });
+    if (content.includes("// BEGIN PEM")) {
+      let lines = content.split(/\r?\n/);
+
+      let pemSection = null;
+      for (const [index, line] of lines.entries()) {
+        if (line.trim() === "// END PEM") {
+          pemSection = null;
+        }
+        if (pemSection !== null) {
+          // Is PEM section
+          if (pemIntegration) {
+            if (line.startsWith("// ")) {
+              lines[index] = lines[index].slice(2);
+            }
+          } else {
+            lines[index] = "// " + lines[index];
+          }
+        }
+        if ((line.trim() === "// BEGIN PEM" || pemSection) && line.trim() !== "// END PEM") {
+          pemSection = true;
+        } else {
+          pemSection = null;
+        }
+      }
+      content = lines.join("\r\n");
+      writeFileSync(file, content, { encoding: "utf-8" });
+    }
+  }
 }
