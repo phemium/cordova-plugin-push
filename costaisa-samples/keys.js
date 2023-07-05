33ffba5d77c79697e20bdfcb12ac1825301d7f24
Phemium <devel@phemium.com>
2023-07-05 12:57:55 +0200

    Remove keys

--- costaisa-samples/keys.js
+++ costaisa-samples/keys.js
@@ -3,14 +3,14 @@
  * Can be obtained from Firebase Devel account in Project Settings --> Cloud Messaging --> Cloud Messaging API --> Server Key
  */
 const API_KEY =
-  "AAAA6Nl-Gqk:APA91bGg4vopDHzYLZj33h3JnrYVSaXrO1CMouCC93a11UpdHLAXgHGzR21B8w9cTtug4a-fF47U52BKT1KyC48NVng1K30izIIZebm9TB0yVv08xTvcw5esdJODJqLqHe6wAsjy4C69";
+  "";
 
 /**
  * Put here the Device unique ID, commonly known as the FCM Token
  * Can be obtained from the corresponding application DevTools Console after Login
  */
 const DEVICE_ID =
-  "dqyIImaD45M:APA91bGOD_ejNwkZoMGsy_B6ncRTr9lDVhTxbTARYwll0ThG9sd9WlgavjXewQ5MCKNysKJpdTJFrNz08J7uqbTBbk_IWCUs1hmQhegNc6r93-jTh2JiCkdaETT0DAEHZ97jGMu4mlo_";
+  "";
 
 module.exports.API_KEY = API_KEY;
 module.exports.DEVICE_ID = DEVICE_ID;
