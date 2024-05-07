797b9098506b734ed378c0fe799ca843a25560fd
Alex Barba Perez <abarbaperez@costaisa.com>
2024-05-07 10:25:05 +0200

    Implement configure Firebase hook

--- costaisa-samples/keys.js
+++ costaisa-samples/keys.js
@@ -3,14 +3,14 @@
  * Can be obtained from Firebase Devel account in Project Settings --> Cloud Messaging --> Cloud Messaging API --> Server Key
  */
 const API_KEY =
-  "";
+  "AAAAcwARiyY:APA91bG3_udH18FMeXfCbUaModv3PnAWuj2gynuW6LhHUBIWOXtqvw7F0iUcWeHLGQf9CSBtkOlWjBIPQInLg_GNOzS-AmEk3ZxFKydS6ZYQ7eddhtFaR3sh_JstaDHSXU9Y4cPb-KZw";
 
 /**
  * Put here the Device unique ID, commonly known as the FCM Token
  * Can be obtained from the corresponding application DevTools Console after Login
  */
 const DEVICE_ID =
-  "";
+  "d8ixShANKac:APA91bEmqNux4ss_SqpdgKSaD0GijIDWvkx6-Ovpj6vo6ToXKGTGBQRo9mkJd6_R7XIxJ0iTXOXEiLvM0qy7VJaThcZ0S1GgHGVIObNBZ5S6MWQnTuk8nlYKb4CcajYOc5YCIMRlLhOC";
 
 module.exports.API_KEY = API_KEY;
 module.exports.DEVICE_ID = DEVICE_ID;
--- hooks/android/beforeCompile.js
+++ hooks/android/beforeCompile.js
@@ -8,6 +8,7 @@ const glob = promisify(_glob);
 
 module.exports = async function (context) {
   await configurePEM(context);
+  configureFirebaseIOS();
   if (!isExecutable()) {
     console.log("[cordova-plugin-push::before-compile] skipping before_compile hookscript.");
     return;
@@ -111,4 +112,34 @@ async function configurePEM(context) {
       writeFileSync(file, content, { encoding: "utf-8" });
     }
   }
+}
+
+function configureFirebaseIOS(context) {
+  console.log("[cordova-plugin-push::before-compile] Importing Firebase...");
+  const appName = getAppName(context);
+
+  const appDelegate = join(context.opts.projectRoot, "platforms/ios", `${appName}/AppDelegate.m`);
+  const appDelegateContent = readFileSync(appDelegate, "utf8");
+  if (!appDelegateContent.includes("[FIRApp configure]")) {
+    const lines = appDelegateContent.split("\n");
+    const importLine = lines.findIndex((line) => line.includes("@implementation AppDelegate"));
+    if (importLine > -1) {
+      lines.splice(importLine, 0, "@import FirebaseCore;");
+      lines.splice(importLine + 1, 0, "");
+    }
+    const configureLine = lines.findIndex((line) => line.includes("self.viewController"));
+    if (configureLine > -1) {
+      lines.splice(configureLine + 1, 0, "\t[FIRApp configure];");
+    }
+    if (importLine > -1 || configureLine > -1) {
+      console.log("[cordova-plugin-push::before-compile] Patching AppDelegate.m...");
+      const newContent = lines.join("\n");
+      writeFileSync(appDelegate, newContent, "utf8");
+    }
+  }
+}
+
+function getAppName(context) {
+  const projectConfig = parseElementtreeSync(join(context.opts.projectRoot, "config.xml"));
+  return projectConfig.find("./name").text;
 }
